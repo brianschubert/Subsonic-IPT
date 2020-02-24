638d3b0814009f7c4a02e32d2c66df93034381be
Brian Schubert <brianm.schubert@gmail.com>
2020-02-24 16:54:33 -0500

    Correct logic for displaying menu title row on LCD

--- src/tui/menu_manager.cpp
+++ src/tui/menu_manager.cpp
@@ -8,44 +8,63 @@
 
 namespace {
 
-void lcd_print_title(SerLCD& lcd, const char* title, bool current);
+void lcd_print_title(SerLCD& lcd, const char* title, bool current, size_t count = 5);
 }
 
 namespace subsonic_ipt {
 
 void MenuManager::refresh_display(SerLCD& lcd)
 {
-    lcd.clear();
     lcd.setCursor(0, 0);
 
-    if (m_menu_count < 3) {
-        char title_row[20];
-        if (m_menu_count == 1) {
-            sprintf(title_row, "       %-5s        ", m_menus[0]->get_menu_name());
-        } else if (m_menu_count == 2) {
-            sprintf(title_row, "   %-5s    %-5s  ", m_menus[0]->get_menu_name(), m_menus[1]->get_menu_name());
-        }
-        lcd.write(title_row);
+    // Write the row of menu title to the display.
+    // We explicitly handle all cases where there are three or less menus being managed.
+    if (m_menu_count == 1) {
+        lcd_print_title(lcd, m_menus[0]->get_menu_name(), true);
+    } else if (m_menu_count == 2) {
+        lcd_print_title(lcd, m_menus[0]->get_menu_name(), m_current_menu == 0);
+        lcd.write(' ');
+        lcd_print_title(lcd, m_menus[1]->get_menu_name(), m_current_menu == 1);
+    } else if (m_menu_count == 3) {
+        lcd_print_title(lcd, m_menus[0]->get_menu_name(), m_current_menu == 0);
+        lcd.write(' ');
+        lcd_print_title(lcd, m_menus[1]->get_menu_name(), m_current_menu == 1);
+        lcd.write(' ');
+        lcd_print_title(lcd, m_menus[2]->get_menu_name(), m_current_menu == 2);
     } else {
-        const bool menu_left = m_current_menu == 0;
-        const bool menu_right = m_current_menu == m_menu_count - 1;
+        // Whether the current menu is the leftmost option.
+        const bool end_left = m_current_menu == 0;
+        // Whether the current menu is the rightmost option.
+        const bool end_right = m_current_menu == m_menu_count - 1;
+        // Whether to print a scroll arrow on the LHS of the display.
+        const bool arrow_left = m_current_menu > 1;
+        // Whether to print a scroll arrow on the RHS of the display
+        const bool arrow_right = m_current_menu < m_menu_count - 2;
 
-        if (m_current_menu > 1) {
+        const size_t center_menu = end_left
+            ? m_current_menu + 1
+            : (end_right ? m_current_menu - 1 : m_current_menu);
+
+        if (arrow_left) {
             lcd.write('<');
         }
-        lcd_print_title(lcd, m_menus[m_current_menu]->get_menu_name(), menu_left);
+        lcd_print_title(lcd, m_menus[center_menu - 1]->get_menu_name(), end_left);
         lcd.write(' ');
-        lcd_print_title(lcd, m_menus[m_current_menu + 1]->get_menu_name(), !(menu_left || menu_right));
+        lcd_print_title(
+            lcd,
+            m_menus[center_menu]->get_menu_name(),
+            !(end_left || end_right),
+            (arrow_left && arrow_right) ? 4 : 5
+        );
         lcd.write(' ');
-        lcd_print_title(lcd, m_menus[m_current_menu + 2]->get_menu_name(), menu_right);
-
-        if (m_menu_count < m_menu_count - 2) {
+        lcd_print_title(lcd, m_menus[center_menu + 1]->get_menu_name(), end_right);
+        if (arrow_right) {
             lcd.write('>');
         }
     }
 
+    // Allow the submenu to produce the output for the remaining rows.
     lcd.setCursor(1, 0);
-
     m_menus[m_current_menu]->refresh_display(lcd);
 }
 
@@ -74,12 +93,12 @@ void MenuManager::interact(const Menu::Input& input)
 
 namespace {
 
-void lcd_print_title(SerLCD& lcd, const char* title, bool current)
+void lcd_print_title(SerLCD& lcd, const char* title, bool current, size_t count)
 {
     if (current) {
         lcd.write('[');
     }
-    lcd.write(title);
+    lcd.write(reinterpret_cast<const uint8_t*>(title), count);
     if (current) {
         lcd.write(']');
     }
