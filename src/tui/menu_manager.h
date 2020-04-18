f13cda594e2383f6bcb0c59a12d0b43cae1c1806
Brian Schubert <brianm.schubert@gmail.com>
2020-04-18 15:43:24 -0400

    Add documentation for menu manager

--- src/tui/menu_manager.h
+++ src/tui/menu_manager.h
@@ -1,6 +1,12 @@
-//
-// Created by brian on 1/21/20.
-//
+/**
+ * menu_manager.h - A collection of menus sharing the same screen.
+ *
+ * Copyright (c) 2020 Brian Schubert
+ *
+ * This file is distributed under the MIT License. If a copy of the
+ * MIT License was not distributed with this file, you can obtain one
+ * at https://opensource.org/licenses/MIT.
+ */
 
 #ifndef SUBSONIC_IPT_MENU_MANAGER_H
 #define SUBSONIC_IPT_MENU_MANAGER_H
@@ -19,10 +25,23 @@ class MenuManager : public Menu {
     // Implementation not designed with the possibility of zero menus in mind.
     static_assert(S != 0);
 
+    /**
+     * The maximum length of the menu titles that will be displayed by this
+     * menu manager.
+     *
+     * Titles that are longer than this length will be truncated when displayed.
+     */
     constexpr static inline size_t MAX_MENU_NAME_LEN = 5;
 
+    /**
+     * The menus contained in this menu manager.
+     */
     Menu* m_menus[S]{};
 
+    /**
+     * The index of the menu that is currently being displayed by this menu
+     * manager.
+     */
     size_t m_current_menu{0};
 
   public:
@@ -32,6 +51,9 @@ class MenuManager : public Menu {
     }
 
     [[nodiscard]]
+    /**
+     * The number of menus contained in this menu manager
+     */
     constexpr size_t size() const noexcept
     {
         return S;
@@ -47,6 +69,9 @@ class MenuManager : public Menu {
 
     void refresh_display(SerLCD& lcd) override
     {
+        // Clear and refresh the screen if either the state of this menu manager
+        // has changed or if the state of the currently displayed menu has
+        // changed.
         if (m_content_changed || m_menus[m_current_menu]->content_changed()) {
             m_content_changed = false;
             lcd.clear();
@@ -55,6 +80,10 @@ class MenuManager : public Menu {
     }
 
   private:
+    /**
+     * Helper function to print the titles of the menus contained in this menu
+     * manager.
+     */
     static void lcd_print_title(SerLCD& lcd, const char* title, bool current, size_t count = MAX_MENU_NAME_LEN)
     {
         char title_buff[MAX_MENU_NAME_LEN + 1];
@@ -68,6 +97,10 @@ class MenuManager : public Menu {
         }
     }
 
+    /**
+     * Writes this menu manager's title bar to the given LCD and calls the
+     * refresh display function of the currently active menu.
+     */
     void force_refresh_display(SerLCD& lcd);
 
 };
@@ -135,8 +168,8 @@ void MenuManager<S>::force_refresh_display(SerLCD& lcd)
         lcd,
         m_menus[center_menu]->get_menu_name(),
         !(end_left || end_right),
-//        (arrow_left && arrow_right) ? 4 : 5 // Temporarily disable variable width menu titles.
-    5
+//        (arrow_left && arrow_right) ? MAX_MENU_NAME_LEN - 1 : MAX_MENU_NAME_LEN // Temporarily disable variable width menu titles.
+        MAX_MENU_NAME_LEN
     );
     lcd.print(' ');
     lcd_print_title(lcd, m_menus[center_menu + 1]->get_menu_name(), end_right);
