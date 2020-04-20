4c40474d96f1d72296f3eedd0af38d2882770dd7
Brian Schubert <brianm.schubert@gmail.com>
2020-04-20 11:24:59 -0400

    Update copyright in sketch.cpp

--- sketch.cpp
+++ sketch.cpp
@@ -1,7 +1,7 @@
 /**
  * sketch.cpp - Arduino sketch entry point
  *
- * Copyright (c) 2019 Brian Schubert
+ * Copyright (c) 2019, 2020 Brian Schubert
  *
  * This file is distributed under the MIT License. If a copy of the
  * MIT License was not distributed with this file, you can obtain one
